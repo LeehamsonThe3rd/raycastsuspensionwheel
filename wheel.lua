dde0361d3f562ba90cb43be7e96ec5b4a4d6e07b
Liam O'Keefe <62216231+LeehamsonThe3rd@users.noreply.github.com>
2023-01-31 11:04:23 -0800

    Create wheel.lua

---
+++ wheel.lua
@@ -0,0 +1,75 @@
+local Wheel = {}
+Wheel.__index = Wheel
+
+local UP = Vector3.yAxis
+local ZERO = Vector3.zero
+local RAYPARAMS = RaycastParams.new()
+
+function Wheel.new(Component : Part, Chassis : Part)
+	local self = {}
+	
+	self.Chassis = Chassis
+	self.Component = Component
+	self.Force = Component:FindFirstChildOfClass("VectorForce")
+	
+	self.Offset = self.Component.Weld.C0.Position
+	self.WheelRadius = self.Component.Size.Y/2
+	
+	self.SuspensionRay = nil
+	self.SpringLength = 0
+	self.StiffnessForce = 0
+	self.DamperForce = 0
+	self.SuspensionLength = 0
+	self.Velocity = 0
+	self.XForce = 0
+	self.ZForce = 0
+	self.GravityForce = 0
+	self.SuspensionForce = ZERO
+	self.RotationOnlyWheelDirectionCFrame = ZERO
+	
+	RAYPARAMS.FilterDescendantsInstances = {self.Chassis}
+	
+	setmetatable(self, Wheel)	
+	return self
+end
+
+function Wheel:Update(DeltaTime)
+	local SuspensionRayOrigin = (self.Chassis.CFrame*CFrame.new(-self.Offset+(self.WheelRadius*UP))).Position
+	local SuspensionRayDirection = -self.Chassis.CFrame.UpVector*(self.WheelRadius+self.Component:GetAttribute("MaxSuspensionLength"))
+	
+	self.SuspensionRay = workspace:Raycast(
+		SuspensionRayOrigin, 
+		SuspensionRayDirection, 
+		RAYPARAMS
+	)
+	
+	local DeltaTime = math.clamp(DeltaTime,0.00000000001,math.huge)
+
+	if self.SuspensionRay then 
+		self.SpringLength = math.clamp(self.SuspensionRay.Distance - self.WheelRadius, 0, self.Component:GetAttribute("MaxSuspensionLength"))
+		self.StiffnessForce = self.Component:GetAttribute("SpringStiffness") * (self.Component:GetAttribute("MaxSuspensionLength") - self.SpringLength)
+		self.DamperForce = self.Component:GetAttribute("DamperStiffness") * ((self.SuspensionLength-self.SpringLength)/DeltaTime)
+		self.SuspensionForce = UP * (self.StiffnessForce+self.DamperForce)
+		
+		self.RotationOnlyWheelDirectionCFrame = CFrame.lookAt(ZERO, self.Chassis.CFrame.LookVector, self.Chassis.CFrame.UpVector)
+		
+		self.Velocity = self.RotationOnlyWheelDirectionCFrame:VectorToObjectSpace(self.Chassis:GetVelocityAtPosition(self.SuspensionRay.Position))
+		self.XForce = self.RotationOnlyWheelDirectionCFrame.RightVector * -self.Velocity.X * self.Component:GetAttribute("WheelFriction")
+		self.ZForce = self.RotationOnlyWheelDirectionCFrame.LookVector * self.Velocity.Z * self.Component:GetAttribute("WheelFriction")/3
+		self.GravityForce = workspace.Gravity * self.SuspensionRay.Normal * 100
+		
+		self.SuspensionLength = self.SpringLength
+		self.Force.Force = self.SuspensionForce + self.XForce + self.ZForce + self.GravityForce
+		
+		if not self.Component:GetAttribute("SuspensionVisible") then return end
+		self.Component.Weld.C0 = CFrame.new(self.Offset + self.SuspensionRay.Distance*UP - self.WheelRadius*UP*2)
+	else
+		self.SuspensionLength = self.Component:GetAttribute("MaxSuspensionLength")
+		self.Force.Force = ZERO
+		
+		if not self.Component:GetAttribute("SuspensionVisible") then return end
+		self.Component.Weld.C0 = CFrame.new(self.Offset + (self.WheelRadius+self.Component:GetAttribute("MaxSuspensionLength"))*UP - self.WheelRadius*UP*2)
+	end
+end
+
+return Wheel
