088009d29bfa97851eaffee6d67fb42ce2e59cf3
Liam O'Keefe <62216231+LeehamsonThe3rd@users.noreply.github.com>
2023-05-29 14:41:16 -0700

    Update wheel.lua
    rewrite

--- wheel.lua
+++ wheel.lua
@@ -1,75 +1,96 @@
-local Wheel = {}
-Wheel.__index = Wheel
+local WheelModule = {}
+WheelModule.__index = WheelModule
 
-local UP = Vector3.yAxis
-local ZERO = Vector3.zero
-local RAYPARAMS = RaycastParams.new()
-
-function Wheel.new(Component : Part, Chassis : Part)
-	local self = {}
-
-	self.Chassis = Chassis
-	self.Component = Component
-	self.Force = Component:FindFirstChildOfClass("VectorForce")
-
-	self.Offset = self.Component.Weld.C0
-	self.WheelRadius = self.Component.Size.Y/2
-
-	self.SuspensionRay = nil
-	self.SpringLength = 0
-	self.StiffnessForce = 0
-	self.DamperForce = 0
-	self.SuspensionLength = 0
-	self.Velocity = 0
-	self.XForce = 0
-	self.ZForce = 0
-	self.GravityForce = 0
-	self.SuspensionForce = ZERO
-	self.RotationOnlyWheelDirectionCFrame = ZERO
-
-	RAYPARAMS.FilterDescendantsInstances = {self.Chassis}
-
-	setmetatable(self, Wheel)	
+function WheelModule.new(VehicleModel : Model, Model : Model)
+	local self = setmetatable({}, WheelModule)
+	
+	self.SuspensionHeight  = Model:GetAttribute("SuspensionHeight")
+	self.SuspensionDamping = Model:GetAttribute("SuspensionDamping")
+	self.SuspensionSpring  = Model:GetAttribute("SuspensionSpring")
+	self.RollingResistance = Model:GetAttribute("RollingResistance")
+	
+	self.Debug = Model:GetAttribute("Debug")
+	
+	self.Vehicle            = VehicleModel
+	
+	self.Model              = Model
+	self.Weld               = self.Model.Weld
+	self.Attachment         = self.Model.Attachment
+	self.VectorForce        = self.Attachment.VectorForce
+	self.RaycastParams      = RaycastParams.new()
+	self.Raycast            = nil
+	
+	self.SuspensionForce         = Vector3.zero
+	self.SuspensionFrictionForce = Vector3.zero
+	self.ResistanceForce         = Vector3.zero
+	self.FrictionForce           = Vector3.zero
+	
+	self.Offset = self.Weld.C1
+	
+	self.RaycastParams.FilterDescendantsInstances = {VehicleModel}
+	
+	if self.Debug then
+		self.UpIndicator         = Instance.new("ConeHandleAdornment", workspace)
+		self.UpIndicator.Adornee = workspace.Terrain
+	end
+	
 	return self
 end
 
-function Wheel:Update(DeltaTime)
-	local SuspensionRayOrigin = self.Chassis.CFrame:ToWorldSpace(self.Offset:Inverse()).Position
-	local SuspensionRayDirection = -self.Chassis.CFrame.UpVector*(self.Component:GetAttribute("MaxSuspensionLength"))
+function WheelModule:Update(...)
+	self.SuspensionHeight   = self.Model:GetAttribute("SuspensionHeight")
+	self.SuspensionDamping  = self.Model:GetAttribute("SuspensionDamping")
+	self.SuspensionSpring   = self.Model:GetAttribute("SuspensionSpring")
+	self.RollingResistance  = self.Model:GetAttribute("RollingResistance")
+	
+	self:CalculatePhysics(...)
+	self:ApplyForce()
+	self:UpdateModel()
+	
+	self.Debug = self.Model:GetAttribute("Debug")
+	self:UpdateDebug()
+end
 
-	self.SuspensionRay = workspace:Raycast(
-		SuspensionRayOrigin, 
-		SuspensionRayDirection, 
-		RAYPARAMS
-	)
+local function AbsoluteVector(v : Vector3)
+	return Vector3.new(math.abs(v.X),math.abs(v.Y),math.abs(v.Z))
+end
 
-	local DeltaTime = math.clamp(DeltaTime,0.00000000001,math.huge)
-
-	if self.SuspensionRay then 
-		self.SpringLength = math.clamp(self.SuspensionRay.Distance - self.WheelRadius, 0, self.Component:GetAttribute("MaxSuspensionLength"))
-		self.StiffnessForce = self.Component:GetAttribute("SpringStiffness") * (self.Component:GetAttribute("MaxSuspensionLength") - self.SpringLength)
-		self.DamperForce = self.Component:GetAttribute("DamperStiffness") * ((self.SuspensionLength-self.SpringLength)/DeltaTime)
-		self.SuspensionForce = UP * (self.StiffnessForce+self.DamperForce) * self.Chassis:GetMass()
-
-		self.RotationOnlyWheelDirectionCFrame = CFrame.lookAt(ZERO, self.Chassis.CFrame.LookVector, self.Chassis.CFrame.UpVector)
-
-		self.Velocity = self.RotationOnlyWheelDirectionCFrame:VectorToObjectSpace(self.Chassis:GetVelocityAtPosition(self.SuspensionRay.Position))
-		self.XForce = self.RotationOnlyWheelDirectionCFrame.RightVector * -self.Velocity.X * (self.Component:GetAttribute("WheelFriction")*self.Chassis:GetMass())
-		self.ZForce = self.RotationOnlyWheelDirectionCFrame.LookVector * self.Velocity.Z * (self.Component:GetAttribute("WheelFriction")*self.Chassis:GetMass()/3)
-		self.GravityForce = -workspace.Gravity * self.SuspensionRay.Normal * 100
-
-		self.SuspensionLength = self.SpringLength
-		self.Force.Force = self.SuspensionForce + self.XForce + self.ZForce + self.GravityForce
-
-		if not self.Component:GetAttribute("SuspensionVisible") then return end
-		self.Component.Weld.C1 = CFrame.new((-self.WheelRadius + self.SuspensionRay.Distance) * -UP)
-	else
-		self.SuspensionLength = self.Component:GetAttribute("MaxSuspensionLength")
-		self.Force.Force = ZERO
-
-		if not self.Component:GetAttribute("SuspensionVisible") then return end
-		self.Component.Weld.C1 = CFrame.new((-self.WheelRadius + self.Component:GetAttribute("MaxSuspensionLength"))*-UP)
+function WheelModule:CalculatePhysics(dt)
+	self.Raycast = workspace:Raycast(self.Model.Position-(Vector3.yAxis*self.Weld.C1.Position), -self.Vehicle.PrimaryPart.CFrame.UpVector*self.SuspensionHeight, self.RaycastParams)
+	
+	if self.Raycast then 
+		local Extension        = self.SuspensionHeight-self.Raycast.Distance
+		local RelativeVelocity = self.Vehicle.PrimaryPart.CFrame:VectorToObjectSpace(self.Model.Velocity)
+		
+		self.SuspensionForce         = self.Vehicle.PrimaryPart.CFrame.UpVector*(self.SuspensionSpring*Extension-self.SuspensionDamping*RelativeVelocity.Y)
+		self.SuspensionFrictionForce = self.SuspensionForce*-AbsoluteVector(self.Vehicle.PrimaryPart.CFrame:VectorToObjectSpace(Vector3.new(self.Model.CustomPhysicalProperties.Friction,0,self.RollingResistance)))
+		self.ResistanceForce         = self.Vehicle.PrimaryPart.CFrame.LookVector*(((RelativeVelocity.Z*self.Model.AssemblyMass))*self.RollingResistance)
+		self.FrictionForce           = -self.Vehicle.PrimaryPart.CFrame.RightVector*(((RelativeVelocity.X*self.Model.AssemblyMass))*self.Model.CustomPhysicalProperties.Friction)
 	end
 end
 
-return Wheel
+function WheelModule:ApplyForce()
+	if not self.Raycast then self.VectorForce.Force = Vector3.zero return end
+	self.VectorForce.Force = self.SuspensionForce + self.SuspensionFrictionForce + self.ResistanceForce + self.FrictionForce
+end
+
+function WheelModule:UpdateModel()
+	local RelativeVelocity = self.Vehicle.PrimaryPart.CFrame:VectorToObjectSpace(self.Model.Velocity)
+	
+	self.Weld.C1 = self.Offset * CFrame.new(0,-self.SuspensionHeight+(self.Model.Size.Y/2),0)
+	if self.Raycast then self.Weld.C1 = self.Offset * CFrame.new(0,-self.Raycast.Distance+(self.Model.Size.Y/2),0) end
+	self.Weld.C0 *= CFrame.Angles(math.rad(-RelativeVelocity.Z/(self.Model.Size.Y/2)),0,0)
+end
+
+function WheelModule:UpdateDebug()
+	if not self.Debug then return end
+	
+	self.UpIndicator.CFrame = CFrame.new(self.Model.Position+(Vector3.yAxis*5)) * CFrame.Angles(math.rad(90),0,0)
+	if self.Raycast then
+		self.UpIndicator.CFrame = CFrame.new(self.Model.Position+(Vector3.yAxis*5)) * CFrame.lookAt(Vector3.zero, self.SuspensionForce.Unit)
+	end
+	
+	print("\n	[Suspension]: "..tostring(self.SuspensionForce), "\n	[SuspensionFriction]: "..tostring(self.SuspensionFrictionForce), "\n	[Resistance]: "..tostring(self.ResistanceForce), "\n	[Friction]: "..tostring(self.FrictionForce), "\n	[NET]: "..tostring(self.VectorForce.Force))
+end
+
+return WheelModule
